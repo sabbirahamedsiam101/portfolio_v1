89d88055ec5e5c74b698cf385c8e7f4d81b1c1c7
sabbirahamedsiam101 <sabbirahamedsiam101@gmail.com>
2024-05-26 07:14:28 +0600

    ass more responsive in navber

--- src/Componets/Nav.jsx
+++ src/Componets/Nav.jsx
@@ -43,19 +43,19 @@ export default function Nav() {
 
     return (
         <div className="navbar bg-base-100 mt-7 px-[5%]">
-            <div className="sm:navbar-start navbar-end">
+            <div className="navbar-start ">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                       {
-                        links
-                       }
+                        {
+                            links
+                        }
                     </ul>
                 </div>
-            </div>
                 <a className="navbar-start text-4xl  font-bold">SABBIR </a>
+            </div>
             <div className="navbar-center hidden px-1 bg-black rounded-full md:flex">
                 <ul className="menu menu-horizontal px-1">
                     {
